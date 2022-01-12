a3516531770800d83396c2bd4386f63806677b99
devrayhankabir <devrayhankabir@gmail.com>
2022-01-13 03:24:07 +0600

    assets loaded

---
+++ website/resources/views/Layout/app.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+    <title>Laravel Portfolio Website</title>
+
+    <meta name="description" content="Let's make your dream website with rkDynamic">
+    <meta name="keywords" content="Let's make your dream website with rkDynamic">
+    <meta name="author" content="rkDynamic">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/css2?family=Hind+Siliguri:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+ 
+    <link href="{{asset('css/bootstrap.min.css')}}" rel="stylesheet" >
+    <link href="{{asset('css/custom.css')}}" rel="stylesheet" >
+    <link href="{{asset('css/responsive.css')}}" rel="stylesheet" >
+    <link href="{{asset('css/owl.carousel.min.css')}}" rel="stylesheet" >
+    <link href="{{asset('css/fontawesome.css')}}" rel="stylesheet">
+    <link href="{{asset('css/animate.css')}}" rel="stylesheet">
+</head>
+<body>
+    
+
+
+    <script type="text/javascript" src="{{asset('js/jquery-3.4.1.min.js')}}"></script>
+    <script type="text/javascript" src="{{asset('js/popper.min.js')}}"></script>
+    <script type="text/javascript" src="{{asset('js/bootstrap.min.js')}}"></script>
+    <script type="text/javascript" src="{{asset('js/owl.carousel.min.js')}}"></script>
+    <script type="text/javascript" src="{{asset('js/axios.min.js')}}"></script>
+    <script type="text/javascript" src="{{asset('js/custom.js')}}"></script>
+</body>
+</html>
